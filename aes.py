dde6ea06d68d233e8b1fea61062d85d01677a7c9
bryanherger <bryanherger@gmail.com>
2022-08-24 03:50:51 -0400

    add perf testing

--- aes.py
+++ aes.py
@@ -1,6 +1,7 @@
 from os import urandom
 from Crypto.Cipher import AES
 import base64
+import sys
 
 # For Generating cipher text
 secret_key = b'0123456789abcdef'
@@ -8,7 +9,12 @@ iv = b'0123456789abcdef'
 obj = AES.new(secret_key, AES.MODE_CBC, iv)
 
 # Encrypt the message
-message = b'Lorem Ipsum text'
+message = 'Lorem Ipsum text'
+if len(sys.argv) > 1:
+    message = sys.argv[1]
+while (len(message) % 16 != 0):
+    message = message + "."
+message = message.encode('utf-8')
 print('Original message is: ', message)
 encrypted_text = obj.encrypt(message)
 print('The encrypted text', encrypted_text)
--- ddl/install.sql
+++ ddl/install.sql
@@ -8,5 +8,28 @@ CREATE FUNCTION aes_decrypt AS NAME 'aes_decrypt_factory' LIBRARY PyAesScalarFun
 -- Step 3: Test
 SELECT aes_decrypt('e8PEhlVv2+nbY/YsU/8vSQ==');
 
--- Step 4: Clean up
+-- Step 4: Timing
+\timing
+CREATE TABLE tempaestester (enc VARCHAR(100));
+INSERT INTO tempaestester VALUES ('uWTpHwuMurTfMPHDVbRsI+ExHi3XxtIMNWN5m59ONA02rf2KVaxGAytlMJ/9CykL+c9ODvLGzD66ibXwvPF6xw==');
+INSERT INTO tempaestester VALUES ('e8PEhlVv2+nbY/YsU/8vSQ==');
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+INSERT INTO tempaestester SELECT * FROM tempaestester;
+SELECT COUNT(*) FROM tempaestester;
+\o /dev/null
+SELECT aes_decrypt(enc) FROM tempaestester;
+SELECT aes_decrypt(enc) FROM tempaestester;
+DROP TABLE tempaestester;
+
+-- Step 5: Clean up
 DROP LIBRARY PyAesScalarFunctions CASCADE;
